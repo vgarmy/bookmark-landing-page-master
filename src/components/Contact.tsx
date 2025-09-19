38789801cd1fffd2ee8b64c336fe658653eae854
Vladimir Gatara <vladimir.gatara@carbiotix.com>
2025-09-19 09:05:13 +0200

    contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 function Contact() {
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
+  const [sent, setSent] = useState("");
 
   const validateEmail = (email: string) => {
     // Basic email regex
@@ -13,12 +14,12 @@ function Contact() {
     e.preventDefault();
 
     if (!email) {
-      setError("Email is required.");
+      setError("Whoops, make sure it's an email");
     } else if (!validateEmail(email)) {
-      setError("Please enter a valid email address.");
+      setError("Whoops, make sure it's an email");
     } else {
       setError("");
-      console.log("Submitted email:", email);
+      setSent("Submitted email:" + email);
       setEmail(""); // reset input
     }
   };
@@ -29,23 +30,40 @@ function Contact() {
         <p className="text-xs uppercase text-white">35,000+ already joined</p>
         <p className="text-3xl text-white">Stay up-to-date with what we’re doing</p>
 
-        <form className="w-full" onSubmit={handleSubmit}>
-          <div className="flex flex-col md:flex-row gap-2">
-            <input
-              type="email"
-              placeholder="Enter your email address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className={`flex-[5] px-4 py-2 rounded border bg-white border-gray-300 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400`}
-            />
+        <form className="w-full" onSubmit={handleSubmit} noValidate>
+          <div className="flex flex-col md:flex-row gap-2 relative">
+            <div className="flex-[5] relative">
+              <input
+                type="text"
+                placeholder="Enter your email address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className={`w-full px-4 py-2 rounded placeholder:text-xs bg-white placeholder-gray-300 focus:outline-none
+        ${error
+                    ? "border-2 border-red-500"
+                    : "border-2 border-white focus:ring-2 focus:ring-blue-400"}`}
+              />
+              {error && (
+                <>
+                  <span className="absolute left-0 right-0 -bottom-[30px] h-[30px] bg-red-500 rounded-b"></span>
+                  <p className="absolute left-2 -bottom-[26px] text-white italic text-xs">{error}</p>
+                </>
+              )}
+               {sent && (
+                <>
+                  <p className="absolute left-2 -bottom-[26px] text-white italic text-xs">{sent}</p>
+                </>
+              )}
+            </div>
+
             <button
               type="submit"
-              className="text-xs flex-[2] border bg-[var(--Red-400)] hover:bg-white hover:text-[var(--Red-400)] text-white px-6 py-2 rounded cursor-pointer hover:border-1 border-[var(--Red-400)]"
+              className="text-xs flex-[2] border bg-[var(--Red-400)] hover:bg-white hover:text-[var(--Red-400)] text-white px-6 py-2 rounded cursor-pointer border-[var(--Red-400)]"
             >
               Contact Us
             </button>
           </div>
-          {error && <p className="mt-2 text-red-500 text-sm">{error}</p>}
+
         </form>
       </div>
     </section>
